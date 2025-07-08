dc44c48621cb74a86b5c867638638ffb7b1a3be5
Javier Placencia <javix.plax@gmail.com>
2025-07-07 23:20:34 -0400

    Colores azulados petroleo, items presionables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://javopa.github.io',
+  site: 'https://javopa.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
---
+++ src/constants/pageTitles.ts
@@ -0,0 +1,3 @@
+export const fixedTitle: string = 'Portfolio de Javier Placencia'
+
+export const Contacto: string = `Contacto - ${fixedTitle}`
